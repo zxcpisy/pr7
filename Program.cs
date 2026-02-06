374bd28d3713f5bee4a7d44f5cbdabe514ce0a78
Pleshz <pleskovd96@gmail.com>
2026-02-06 09:27:16 +0500

    Получение информации с сайта. Kanyukov

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,17 @@ namespace pr7kankan
     {
         static void Main(string[] args)
         {
+            WebRequest request = WebRequest.Create("");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string  resposeFromServer = reader.ReadToEnd();
+            Console.WriteLine(resposeFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
